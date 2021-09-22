a2752d48cfaa9fd9aae1c557f722a00c7251e5de
hannarosenfeld <hannarosenfeld@gmail.com>
2021-09-22 10:08:23 -0400

    Add behavior when hovering over logos.

--- hzr-additions.js
+++ hzr-additions.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {          
 
+    /* When hovering over images, give logos a border*/    
     $('.goal-item').hover(function(){
         var Color = $(this).attr("data-color");
         var Relative = "." + $(this).attr("data-id");
@@ -10,4 +11,20 @@ $(document).ready(function() {
                               $(Relative).css("border","none");
                           }
                          );
+
+
+    /* When hovering over logos, give images a border and highlight container */    
+    $('.org-logo').hover(function(){
+        var Relative = "." + $(this).attr("data-id");
+        $(Relative).css('border', '4px solid white');
+        $(Relative).parent().parent().parent().parent().css("filter", "saturate(1.5)");
+    },  
+                         function () {
+                             var Relative = "." + $(this).attr("data-id");
+                             $(Relative).css("border","none");
+                             $(Relative).parent().parent().parent().parent().css("filter", "none");
+                         }
+                        );
+    
+    /* Written by hannazrosenfeld@gmail.com (September 2021) */
 });
