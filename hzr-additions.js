7dae22b584a8fb345c79015faf3ce5183d511b02
hannarosenfeld <hannarosenfeld@gmail.com>
2021-09-22 13:25:36 -0400

    Refactoring.

--- hzr-additions.js
+++ hzr-additions.js
@@ -2,27 +2,27 @@ $(document).ready(function() {
 
     /* When hovering over images, give logos a border*/    
     $('.goal-item').hover(function(){
-        var Color = $(this).attr("data-color");
-        var Relative = "." + $(this).attr("data-id");
-        $(Relative).css('border', '4px solid' + Color);
+        var imageColor = $(this).attr("data-color");
+        var relatedLogo = "." + $(this).attr("data-id");
+        $(relatedLogo).css('border', '4px solid' + imageColor);
     },  
                           function () {
-                              var Relative = "." + $(this).attr("data-id");
-                              $(Relative).css("border","none");
+                              var relatedLogo = "." + $(this).attr("data-id");
+                              $(relatedLogo).css("border","none");
                           }
                          );
 
 
     /* When hovering over logos, give images a border and highlight container */    
     $('.org-logo').hover(function(){
-        var Relative = "." + $(this).attr("data-id");
-        $(Relative).css('border', '4px solid white');
-        $(Relative).parent().parent().parent().parent().css("filter", "saturate(1.5)");
+        var relatedImage = "." + $(this).attr("data-id");
+        $(relatedImage).css('border', '4px solid white');
+        $(relatedImage).parents('.panel').css("filter", "saturate(1.5)");
     },  
                          function () {
-                             var Relative = "." + $(this).attr("data-id");
-                             $(Relative).css("border","none");
-                             $(Relative).parent().parent().parent().parent().css("filter", "none");
+                             var relatedImage = "." + $(this).attr("data-id");
+                             $(relatedImage).css("border","none");
+                             $(relatedImage).parents('.panel').css("filter", "none");
                          }
                         );
     
