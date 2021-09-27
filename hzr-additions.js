88650505ecb6c8a8d87703cf53e26a509993fa57
hannarosenfeld <hannarosenfeld@gmail.com>
2021-09-27 11:49:28 -0400

    Refactor

--- hzr-additions.js
+++ hzr-additions.js
@@ -1,30 +1,27 @@
 $(document).ready(function() {          
-
     /* When hovering over images, give logos a border*/    
     $('.goal-item').hover(function(){
         var imageColor = $(this).attr("data-color");
         var relatedLogo = "." + $(this).attr("data-id");
-        $(relatedLogo).css('border', '4px solid' + imageColor);
+        $(relatedLogo).css('box-shadow', '0px 0px 0px 4px' + imageColor);
     },  
                           function () {
                               var relatedLogo = "." + $(this).attr("data-id");
-                              $(relatedLogo).css("border","none");
+                              $(relatedLogo).css("box-shadow","none");
                           }
                          );
 
-
     /* When hovering over logos, give images a border and highlight container */    
-    $('.org-logo').hover(function(){
+    $(".org-logo").hover(function(){
         var relatedImage = "." + $(this).attr("data-id");
-       $(relatedImage).css('box-shadow', '0px 0px 0px 4px white');
-        $(relatedImage).parents('.panel').css("filter", "saturate(1.5)");
+        $(relatedImage).addClass("image-border");
+        $(relatedImage).parents(".row").addClass("panel-saturate");
     },  
                          function () {
                              var relatedImage = "." + $(this).attr("data-id");
-                             $(relatedImage).css("box-shadow","none");
-                             $(relatedImage).parents('.panel').css("filter", "none");
-                         }
-                        );
+                             $(relatedImage).removeClass("image-border");
+                             $(relatedImage).parents(".row").removeClass("panel-saturate");
+                         });
     
     /* Written by hannazrosenfeld@gmail.com (September 2021) */
 });
